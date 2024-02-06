c518ddbaaf8c3ced9f8f3424b5feec851f56a085
aymen-000 <aymen011@gmail.com>
2024-02-06 23:40:35 +0100

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Route ,Routes , BrowserRouter  } from 'react-router-dom'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-import { store } from './redux/store.js'
+import { store, persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Header/>
-      <App/>
-      <Footer/>
-    </BrowserRouter>
-  </Provider>,
+  <PersistGate persistor={persistor}>
+    <Provider store={store} >
+      <BrowserRouter>
+        <Header />
+        <App />
+        <Footer />
+      </BrowserRouter>
+    </Provider>,
+  </PersistGate>
 )
